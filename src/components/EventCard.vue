38e30990440dbb5d8702a93fa19f19aa3abe3a25
Ndi Britha Lea <leadelve@gmail.com>
2020-04-13 15:21:29 +0100

    event cards display

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,54 +1,38 @@
 <template>
-  <div class="event-card">
-    <div class="card">
-      <div class="card-content">
-        <h2 class="is-size-4 has-text-weight-bold">Event name</h2>
-        <small class="event-date">Event date</small>
-        <span>Event location</span>
-      </div>
+  <div class="card mb-4 text-white">
+        <img class="card-img" :src="event.featuredImage" alt="Card image">
+        <div class="card-img-overlay">
+            <h5 class="card-title my-5">{{event.name}}</h5>
+            <p class="card-text">{{event.description}}</p>
+            <small class="event-date">{{event.date}}</small>
+        </div>
     </div>
-  </div>
 </template>
 <script>
   export default {
+    props: [
+      'event'
+    ]
   }
 </script>
 <style lang="scss" scoped>
-  .card {
-    background-image: url('https://placekitten.com/400/400');
-    height: 200px;
-    background-position: center;
-    background-size: cover;
-    text-align: center;
-  }
-  .card-content {
-    padding-top: 50px;
-    position: absolute;
-    color: #FFF;
-    background-color: rgba(0, 0, 0, 0.35);
-    top: 0;
-    padding: 10px;
-    height: 200px;
-    width: 100%;
-    span {
-      font-size: 18px;
-      text-align: center;
-      width: 100%;
-      position: absolute;
-      bottom: 10px;
-      right: 0;
-    }
-    h2 {
-      margin-top: 10px;
-    }
-  }
+.card{
+  width: 350px !important;
+}
   .event-date {
-    background-color: #151515;
+    background-color: #28a745;
     color: #FFF;
     font-size: .75em;
     padding: 2px 10px;
     position: absolute;
     top: 0;
     right: 0;
+  }
+  .card-img-overlay{
+      background-color: #1613132f;
+  }
+  .card-img{
+      height: 300px;
+      width: inherit;
   }
 </style>
--- src/components/EventsList.vue
+++ src/components/EventsList.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="events container">
-    <h2 class="subtitle is-3"> Check out our upcoming events</h2>
-    <div class="columns is-multiline">
-      <div class="column is-one-quarter">
-          <EventCard />
-      </div>
+    <h2 class="mt-5 mb-3 text-bold"> Check out our upcoming events</h2>
+        <div class="row">
+          <div v-for="even in events" :event="even" :key="even.id">        
+            <div class="col-md-4">
+                <router-link :to="'/event/' + even.id">
+                    <EventCard :event="even" />
+                </router-link>
+              </div>
+          </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -15,13 +19,75 @@
     name: 'EventsList',
     components : {
       EventCard
+    },
+    data(){
+        return {
+            event: {},
+            events:[
+                {
+                id: 1,
+                name: 'Charity Ball',
+                category: 'Fundraising',
+                description: 'Spend an elegant night of dinner and dancing with us as we raise money for our new rescue farm.',
+                featuredImage: 'https://placekitten.com/400/300',
+                images: [
+                    'https://placekitten.com/400/400',
+                    'https://placekitten.com/400/400',
+                    'https://placekitten.com/400/400',
+                ],
+                location: '1234 Fancy Ave',
+                date: '12-25-2019',
+                time: '11:30'
+                },
+                              {
+                id: 4,
+                name: 'Charity Ball',
+                category: 'Fundraising',
+                description: 'Spend an elegant night of dinner and dancing with us as we raise money for our new rescue farm.',
+                featuredImage: 'https://placekitten.com/400/500',
+                images: [
+                    'https://placekitten.com/400/400',
+                    'https://placekitten.com/400/400',
+                    'https://placekitten.com/400/400',
+                ],
+                location: '1234 Fancy Ave',
+                date: '12-25-2019',
+                time: '11:30'
+                },
+                              {
+                id: 3,
+                name: 'Charity Ball',
+                category: 'Fundraising',
+                description: 'Spend an elegant night of dinner and dancing with us as we raise money for our new rescue farm.',
+                featuredImage: 'https://placekitten.com/500/500',
+                images: [
+                    'https://placekitten.com/400/400',
+                    'https://placekitten.com/400/400',
+                    'https://placekitten.com/400/400',
+                ],
+                location: '1234 Fancy Ave',
+                date: '12-25-2019',
+                time: '11:30'
+                },
+                {
+                id: 2,
+                name: 'Rescue Center Goods Drive',
+                category: 'Adoptions',
+                description: 'Come to our donation drive to help us replenish our stock of pet food, toys, bedding, etc. We will have live bands, games, food trucks, and much more.',
+                featuredImage: 'https://placekitten.com/500/600',
+                images: [
+                    'https://placekitten.com/500/500'
+                ],
+                location: '1234 Dog Alley',
+                date: '11-21-2019',
+                time: '12:00'
+                }
+            ]
+        }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  .events {
-    margin-top: 100px;
-    text-align: center;
-  }
+ 
 </style>
